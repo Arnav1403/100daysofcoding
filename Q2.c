da346ab42c4119dfca3e7893eb13906f7abbdef8
Arnav1403 <ajgamerz835@gmail.com>
2025-11-03 22:20:52 +0530

    TO input two numbers and display their  sum, diff, product, quotient

---
+++ Q2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    float num1, num2, sum, difference, product, quotient;
+
+    // Input two numbers
+    printf("Enter first number: ");
+    scanf("%f", &num1);
+
+    printf("Enter second number: ");
+    scanf("%f", &num2);
+
+    // Perform operations
+    sum = num1 + num2;
+    difference = num1 - num2;
+    product = num1 * num2;
+
+    // Check for div
