b7e9268718eb45b0ab4c784e40d7ed603da06dfd
Arnav1403 <ajgamerz835@gmail.com>
2025-11-04 15:17:41 +0530

    Find Kth Smallest Element in an Array

---
+++ Q113.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() {
+    int arr[100], n, k, temp;
+
+    printf("Enter number of elements in the array: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    printf("Enter value of k: ");
+    scanf("%d", &k);
+
+    if (k <= 0 || k > n) {
+        printf("Invalid value of k.\n");
+        return 0;
+    }
+
+    // Sort the array in ascending order (using simple Bubble Sort)
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    printf("The %dth smallest element is: %d\n", k, arr[k - 1]);
+
+    return 0;
+}
